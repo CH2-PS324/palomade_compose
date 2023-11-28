bf13b18760a030a42629cd140acf3c93274bc7de
Alfian <alfianfakhrudin11@gmail.com>
2023-11-29 03:02:22 +0700

    nambah navigation bottom

---
+++ app/src/main/java/com/example/palomadeapps/di/Injection.kt
@@ -0,0 +1,10 @@
+package com.example.palomadeapps.di
+
+import android.content.Context
+
+object Injection {
+//    fun provideRepository(context: Context): TrackRepository {
+//        val pref = UserPreference.getInstance(context.dataStore)
+//        return TrackRepository.getInstance(pref)
+//    }
+}
---
+++ app/src/main/java/com/example/palomadeapps/ui/screen/profile/ProfileViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.palomadeapps.ui.screen.profile
+
+import androidx.lifecycle.ViewModel
+import com.example.palomadeapps.data.PaloRepository
+
+class ProfileViewModel(private val repository: PaloRepository): ViewModel(){
+
+}
