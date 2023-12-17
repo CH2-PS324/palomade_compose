335da358d5f699e1489a8f5b50dd08122c7f7eee
Alfian <alfianfakhrudin11@gmail.com>
2023-12-18 01:35:14 +0700

    tambah komponen shipping

--- app/src/main/java/com/example/palomadeapps/ui/components/Shipping.kt
+++ app/src/main/java/com/example/palomadeapps/ui/components/Shipping.kt
@@ -2,27 +2,30 @@ package com.example.palomadeapps.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardOptions
-
-import androidx.compose.material3.*
-
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.palomadeapps.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,18 +37,76 @@ fun Shipping() {
             .fillMaxWidth(),
         onClick = { /* Handle card click */ },
     ) {
-        Column(
+        Row(
             modifier = Modifier
-                .padding(16.dp)
+                .fillMaxWidth()
+                .padding(start = 15.dp, top = 10.dp)
         ) {
-            Text(
-                text = "Card Title",
-                style = MaterialTheme.typography.bodyMedium,
+            Image(
+                painter = painterResource(id = R.drawable.shipping),
+                contentDescription = null,
                 modifier = Modifier
-                    .padding(bottom = 8.dp)
+                    .border(
+                        width = 1.dp,
+                        color = Color(0xFF000000),
+                        shape = RoundedCornerShape(size = 4.dp)
+                    )
+                    .size(70.dp)
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(4.dp))
             )
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp, bottom = 10.dp)
+                    .width(225.dp)
+            ) {
+                Text(
+                    text = "Diproses",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .border(
+                            width = 1.dp,
+                            color = Color(0xFF686868),
+                            shape = RoundedCornerShape(size = 4.dp)
+                        )
+                        .width(75.dp)
+                        .height(19.dp)
+                        .background(
+                            color = Color(0xFFEBEBEB),
+                            shape = RoundedCornerShape(size = 4.dp)
+                        )
+                        .padding(start = 8.dp, end = 8.dp)
+                )
+                Text(
+                    text = "Card Description",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = "22 November 2023",
+                    fontSize = 11.sp,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow_end), contentDescription = "gambar arrow",
+                modifier = Modifier
+                    .padding(top = 5.dp)
+            )
+        }
+        Divider(
+            thickness = 1.dp, color = Color.Black,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 15.dp, end = 15.dp)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 15.dp, top = 8.dp)
+        ){
             Text(
-                text = "Card Description",
+                text = "Estimasi tiba: 24 November 2023",
                 style = MaterialTheme.typography.bodyMedium
             )
         }
